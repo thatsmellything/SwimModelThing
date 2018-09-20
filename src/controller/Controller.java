3093e305ef84c7539a2d9b65c270ca03b5d6db46
Jensen Judkins <thesmelliestman@gmail.com>
2018-09-20 07:44:50 -0600

    Initial commit

---
+++ src/controller/Controller.java
@@ -0,0 +1,19 @@
+package controller;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import model.ModelSwim;
+public class Controller
+{
+	private ModelSwim swimmmer;
+	
+	public Controller()
+	{
+		swimmer = new ModelSwimm("Jensen", true, true, 25.19);
+	}
+	
+	public void start()
+	{
+		
+	}
+
+}
---
+++ src/model/ModelSwim.java
@@ -0,0 +1,20 @@
+package model;
+
+public class ModelSwim
+{
+	private String name;
+	private boolean isWet;
+	private boolean isSwoll
+	private double fFreestyle;
+	
+	public ModelSwim()
+	{
+		
+	}
+	public ModelSwim(String name, boolean isWet, boolean isSwoll, double fFreestyle)
+	{
+		this.name = name;
+		
+	}
+
+}
